d288524c2bfad312fa9be0b73548fd4a0e509745
evgeniStas <evgeni_stas@swiego.com>
2018-02-14 09:52:53 +0200

    Wp head

--- wp-content/themes/swiego/index.php
+++ wp-content/themes/swiego/index.php
@@ -19,6 +19,7 @@ $id_page = 51;
         <title><?php echo bloginfo('name'); ?></title>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="<?php echo get_template_directory_uri();?>/js/main.js"></script>
+        <?php wp_head(); ?>
     </head>
     <body>
         <div class="left-menu">
