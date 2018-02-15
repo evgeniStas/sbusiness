7fa9b221ddfc0532460ac8ac1ff450e7e0690e37
evgeniStas <evgeni_stas@swiego.com>
2018-02-15 15:48:52 +0200

    Questions

--- wp-content/themes/swiego/js/main.js
+++ wp-content/themes/swiego/js/main.js
@@ -4,11 +4,11 @@ $(function() {
         var hidden  = $(this).children(".desc").hasClass("hidden");
         if(hidden){
             $(this).children(".desc").removeClass("hidden");
-            $(this).children(".icon").html("[-]");
+            $(this).children().children(".icon").html("[-]");
             $(this).children(".icon").addClass("icon-open");
         }else{
             $(this).children(".desc").addClass("hidden");
-            $(this).children(".icon").html("[+]");
+            $(this).children().children(".icon").html("[+]");
             $(this).children(".icon").removeClass("icon-open");
         }
     });
